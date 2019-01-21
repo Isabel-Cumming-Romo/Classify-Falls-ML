e29bfed8ae050d50d00d2c6ebe991c08405fd658
Isabel-Cumming-Romo <isabel.cumming25@gmail.com>
2019-01-21 16:25:53 -0500

    Update training.py
    updated constant x_num_rows, made FFP-BP funciton, finished second part of FF

--- training.py
+++ training.py
@@ -5,9 +5,11 @@ import math # for sigmoid
 
 	#this is the number of features in the training matrix being read in (in the MATLAB code, is 256)
 	num_features=256;
+	
+	#this is the number of samples (i.e. rows)
+	x_num_rows=3;
 
 	#initialize lambda (if we're using lambda?)
-	
 	
 
 	#initialize lambda
@@ -24,12 +26,10 @@ import math # for sigmoid
 	#Import x (using pandas?)
 	
 	#for now, have X being a matrix that is 600Xnum_features big  filled with 5's
-	X= numpy.full((600, 256), 2)
+	X= numpy.full((x_num_rows, num_features), 2)
 	
 	#Initialize y=zeros(m, 1)
-	
 
-	x_num_rows=3;
 	
 
 # for 1: max_iterarions
@@ -37,44 +37,47 @@ for x in range(max_iterations):
 
     
 	#Then we use a built-in optimizer 
-			#We pass in X, y, the gradients, and potentially the cost function
+			#We pass in X, y, the gradients, and potentially the cost function (FFP_BP)
 			#We should receive updated weights back
 			
 			#model = opt.minimize(fun = CostFunc, x0 = initial_theta, args = (X, y), method = 'TNC', jac = Gradient)
 	
 
 # Compute gradient and cost function 
-
+def FFP_BP (x):
 #FEED-FORWARD PROPAGATION
+	#layer1_activation =concatenate a column of all ones with X. 
+	#all_ones = numpy.ones((x_num_rows,1)) #a column of 1's
+	#layer1_activation= numpy.hstack((all_ones, X))# i.e. add a column of 1's to the front of the X matrix
+	layer1_activation=X; #TODO-temporarily use layer1_activation without the bias (i.e. the column of 1's)
+			#ie each row is a training example. The first column of each row is now a 1.
+			#so you just add a column -like "the one" feature
+			#layer1_activation is our first layer
 		
-		#layer1_activation =concatenate a column of all ones with X. 
-		#all_ones = numpy.ones((x_num_rows,1)) #a column of 1's
-		#layer1_activation= numpy.hstack((all_ones, X))# i.e. add a column of 1's to the front of the X matrix
-		layer1_activation=X; #TODO-temporarily use layer1_activation without the bias (i.e. the column of 1's)
-				#ie each row is a training example. The first column of each row is now a 1.
-				#so you just add a column -like "the one" feature
-				#layer1_activation is our first layer
-			
-		#z_2 = w_1 * layer1_activation	
-		z_2 = numpy.matmul(layer1_activation, w_1) #intermediary variable (note: order is important for the multiplication so that dimensions match up)
+	#z_2 = w_1 * layer1_activation	
+	z_2 = numpy.matmul(layer1_activation, w_1) #intermediary variable (note: order is important for the multiplication so that dimensions match up)
 
-		#Compute layer2_activation = sigmoid(z_2)
-		layer2_activation= sigmoid (z_2)
-			
-		#Compute a_3
-			# Concatenate a bias column of all 1s with layer2_activation	
-			# z_3 = w_2*layer2_activation
-			#  layer3_activation = sigmoid(z_3)
-	
-		#Compute h (output layer activation...ie the hypothesis)
-				#Concatenate bias column of all 1s with layer3_activation
-				# z_out = w_3*layer3_activation
-				# h = sigmoid(z_out)
-
-		#cost= computeCost(X, y, h, m)
-			#m is the number of training examples 
+	#Compute layer2_activation = sigmoid(z_2)
+	layer2_activation= sigmoid (z_2)
 		
-	#BACKPROP
+	#Compute a_3
+		# Concatenate a bias column of all 1s with layer2_activation	
+		#all_ones = numpy.ones((x_num_rows,1)) #column of 1's
+		#layer2_activation= numpy.hstack((all_ones,layer2_activation))# i.e. add a column of 1's to the front of the layer2_activation
+		# z_3 = w_2*layer2_activation
+		z_3= numpy.matmul (layer2_activation, w_2)
+		#  layer3_activation = sigmoid(z_3)
+		layer3_activation = sigmoid(z_3)
+
+	#Compute h (output layer activation...ie the hypothesis)
+			#Concatenate bias column of all 1s with layer3_activation
+			# z_out = w_3*layer3_activation
+			# h = sigmoid(z_out)
+
+	#cost= computeCost(X, y, h, m)
+		#m is the number of training examples 
+		
+#BACKPROP
 		
 		#output_layer_gradient = 2*(output_layer_activation - y)/m
 		
@@ -98,7 +101,7 @@ for x in range(max_iterations):
  
         #[W1_gradient, ~] = compute_gradient(layer1_z_gradient, w_1, X);
 
-
+#end FFP_BP
 	
 #COMPUTE GRADIENT FUNCTION
 
