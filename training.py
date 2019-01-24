3c2ce263d2c2d91dfa11ddabee83a2da15085d3e
Monet Nicole <35469284+MonetSlinky@users.noreply.github.com>
2019-01-24 16:38:23 -0500

    training updates

--- training.py
+++ training.py
@@ -2,48 +2,9 @@
 import random # for w's initalizations
 import numpy # for all matrix calculations
 import math # for sigmoid
+import scipy
+import pandas as pd
 
-	#this is the number of features in the training matrix being read in (in the MATLAB code, is 256)
-	num_features=256;
-	
-	#this is the number of samples (i.e. rows)
-	x_num_rows=3;
-
-	#initialize lambda (if we're using lambda?)
-	
-
-	#initialize lambda
-	lam=0
-	
-	#initialize max number of iterations
-	max_iterations=5
-	
-	# Initialize weights to random numbers: w_1, w_2, w_3 ...# TO DO: make sure the initialization numbers are small (between 0 and 1)
-	w_1= numpy.matrix(numpy.random.random((num_features, num_features))) #for now, since don't know what # of internal nodes will have (i.e. the latter dimension of this matrix), just make it 256
-	w_2= numpy.matrix(numpy.random.random((num_features, num_features)))
-	w_3= numpy.matrix(numpy.random.random((num_features, 1)))
-	
-	#Import x (using pandas?)
-	
-	#for now, have X being a matrix that is 600Xnum_features big  filled with 5's
-	X= numpy.full((x_num_rows, num_features), 2)
-	
-	#Initialize y=zeros(m, 1)
-
-	
-
-# for 1: max_iterarions
-for x in range(max_iterations):
-
-    
-	#Then we use a built-in optimizer 
-			#We pass in X, y, the gradients, and potentially the cost function (FFP_BP)
-			#We should receive updated weights back
-			
-			#model = opt.minimize(fun = CostFunc, x0 = initial_theta, args = (X, y), method = 'TNC', jac = Gradient)
-	
-
-# Compute gradient and cost function 
 def FFP_BP (x):
 #FEED-FORWARD PROPAGATION
 	#layer1_activation =concatenate a column of all ones with X. 
@@ -65,9 +26,9 @@ def FFP_BP (x):
 		#all_ones = numpy.ones((x_num_rows,1)) #column of 1's
 		#layer2_activation= numpy.hstack((all_ones,layer2_activation))# i.e. add a column of 1's to the front of the layer2_activation
 		# z_3 = w_2*layer2_activation
-		z_3= numpy.matmul (layer2_activation, w_2)
+	z_3= numpy.matmul (layer2_activation, w_2)
 		#  layer3_activation = sigmoid(z_3)
-		layer3_activation = sigmoid(z_3)
+	layer3_activation = sigmoid(z_3)
 
 	#Compute h (output layer activation...ie the hypothesis)
 			#Concatenate bias column of all 1s with layer3_activation
@@ -162,3 +123,47 @@ def sigmoid(x):
 			
 		#Return final cost: J= J + regTerm
 				
+
+data = pd.read_csv("CapstoneData_Revised.csv", low_memory=False);
+
+	#this is the number of features in the training matrix being read in (in the MATLAB code, is 256)
+num_features=10299;
+	
+	#this is the number of samples (i.e. rows)
+x_num_rows=647;
+
+X = data.iloc[:, 10299]; #the class labels are the last column of the csv file
+Y=data.iloc[:, 0:10298];
+	
+	#initialize lambda
+lam=0
+	
+	#initialize max number of iterations
+max_iterations=5
+	
+	# Initialize weights to random numbers: w_1, w_2, w_3 ...# TO DO: make sure the initialization numbers are small (between 0 and 1)
+w_1= numpy.matrix(numpy.random.random((num_features, num_features))) #for now, since don't know what # of internal nodes will have (i.e. the latter dimension of this matrix), just make it 256
+w_2= numpy.matrix(numpy.random.random((num_features, num_features)))
+w_3= numpy.matrix(numpy.random.random((num_features, 1)))
+	
+	#Import x (using pandas?)
+	
+	#for now, have X being a matrix that is 600Xnum_features big  filled with 5's
+X= numpy.full((x_num_rows, num_features), 2)
+	
+	#Initialize y=zeros(m, 1)
+
+
+
+# for 1: max_iterarions
+#for x in range(max_iterations):
+
+    
+	#Then we use a built-in optimizer 
+			#We pass in X, y, the gradients, and potentially the cost function (FFP_BP)
+			#We should receive updated weights back
+			
+			#model = opt.minimize(fun = CostFunc, x0 = initial_theta, args = (X, y), method = 'TNC', jac = Gradient)
+FFP_BP(x=X);
+
+# Compute gradient and cost function 
